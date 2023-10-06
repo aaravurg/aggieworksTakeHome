bbac8bb2b40eb8130a0b0cac04b244c772dd64ed
aaravurg <aurgaonkar@ucdavis.edu>
2023-10-05 23:58:50 -0700

    improved home ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,47 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
 export const Home = () => {
-    const [data, setData] = useState([]);
-    
-      useEffect(() => {
-        // Make a GET request to your API endpoint
-        axios.get('http://localhost:3001/review')
-          .then((response) => {
-            // Update the state with the fetched data
-            console.log(response.data);
-            setData(response.data);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      }, []);
-      
-      return (
-        <Container className="mt-5">
-          
-          <Row>
-            
-            
-            <Col md={8}>
-            {data.map((dat, i) => {
-                return (
-                    <Card>
-                    <Card.Img src={data[i].imageUrl} />
+  const [data, setData] = useState([]);
 
-                <Card.Body>
-                  <Card.Title>{data[i].songName}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{data[i].artistName}</Card.Subtitle>
-                  <Card.Text>{data[i].songReview}</Card.Text>
-                  <Card.Text>Release Date: {data[i].releaseDate}</Card.Text>
-                  <Card.Text>Review by: {data[i].userOwner}</Card.Text>
-                </Card.Body>
-              </Card>
-                )
-            })}
-              
-            </Col>
-          </Row>
-        </Container>
-      );
-}
+  useEffect(() => {
+    // Make a GET request to your API endpoint
+    axios
+      .get("http://localhost:3001/review")
+      .then((response) => {
+        // Update the state with the fetched data
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <Container className="mt-5 text-center"> {/* Apply text-center class to center-align content */}
+      <Row>
+        {data.map((item, index) => (
+          <Col md={4} key={index}> {/* Added a unique key */}
+            <Card className="mb-4">
+              <Card.Img src={item.imageUrl} />
+
+              <Card.Body>
+                <Card.Title>{item.songName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {item.artistName}
+                </Card.Subtitle>
+                <Card.Text>{item.songReview}</Card.Text>
+                <Card.Text>Release Date: {item.releaseDate}</Card.Text>
+                <Card.Text>Review by: {item.userOwner}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
